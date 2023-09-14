d18c9dc7174ac57fc1f70991b5bc7267d8f89503
MrComMann <ondra.plicka@seznam.cz>
2023-09-14 15:58:35 +0200

    epic eventy

--- eventy.js
+++ eventy.js
@@ -1,10 +1,20 @@
 const btn = document.getElementById('myBtn')
+const inp = document.getElementById('add')
+const ull = document.getElementById('ul')
+const copier= document.getElementById('copier')
+const copy = document.getElementById('copy')
+const z = document.getElementById('z')
 
 let a = 0;
 
 btn.addEventListener('click', () => {
- console.log("klik");
- a += 1;
- if (a == 10) console.log('ty jsi ale klikač')
+    const li = document.createElement("li")
+    li.innerHTML = inp.value
+    inp.value = ""
+    ull.append(li)
 })
 
+copier.addEventListener('input', () => {
+    copy.innerHTML = copier.value;
+    z.innerHTML = copier.value.length;
+})
