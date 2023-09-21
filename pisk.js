abc1357ee27e36b4c7c6763fc82207be9db43c92
Ondra Plicka <ondra.plicka@seznam.cz>
2023-09-21 23:05:58 +0200

    pisk

--- pisk.js
+++ pisk.js
@@ -1,28 +1,73 @@
 const tile = document.getElementsByTagName('button')
 
-const turnS = document.getElementById('turn');
+const message = document.getElementById('turn');
+
+let c = [
+    'Vyhrál X',
+    'Vyhrál O',
+    'Remíza'
+]
+let b = 0;
+let a = [
+    'X', 'O'
+]
 
 let state = [
-    'Hraje: X', 
-    'Hraje: O',
-    'Vyhrál X',
-    'Vyhrál O'
+    2, 2, 2,
+    2, 2, 2,
+    2, 2, 2
 ]
-let a = 0;
 
-tile.addEventListener('click', ()=>{
-    if (a == 0) {
-        a = 1;
-        turnS.innerHTML = state[1]
-        tile[i].innerHTML = "X"
+function clickTurn(id) {
+    if (state[id - 1] == 2) {
+        tile[id - 1].innerHTML = a[b]
+
+        let winner = b;
+        if (b == 0) {
+            b = 1
+            state[id - 1] = 0
+            message.innerHTML = 'Hraje O'
+        } else {
+            b = 0
+            state[id - 1] = 1
+            winner = checkWin(winner)
+            message.innerHTML = 'Hraje X'
+        }
+        winner = checkWin(winner)
+        if (winner !== false) {
+            message.innerHTML = c[winner];
+        }
+
     }
-    else if (a == 1) {
-        a = 0;
-        turnS.innerHTML = state[0]
-        tile[i].innerHTML = "O"
+
+}
+
+function checkWin(winner) {
+
+    if (
+        (state[0] == state[1] && state[1] == state[2] && state[0] != 2) || //row1
+        (state[3] == state[4] && state[4] == state[5] && state[3] != 2) || //row2
+        (state[6] == state[7] && state[7] == state[8] && state[6] != 2) || //row3
+
+        (state[0] == state[3] && state[3] == state[6] && state[0] != 2) || //column1
+        (state[1] == state[4] && state[4] == state[7] && state[1] != 2) || //column2
+        (state[2] == state[5] && state[5] == state[8] && state[2] != 2) || //column3
+
+        (state[0] == state[4] && state[4] == state[8] && state[0] != 2) || //cross1
+        (state[2] == state[4] && state[4] == state[6] && state[2] != 2)    //cross2
+    ) {
+        for (let x of tile) {
+            x.disabled = true;
+            if (x.innerHTML == 'X') x.style = 'background-color:green'
+            if (x.innerHTML == 'O') x.style = 'background-color:blue'
+        }
+        return winner;
+    } else {
+        for (const x of state) {
+            if (x == 2) {
+                return false;
+            }
+        }
+        return 2;
     }
-})
-
-function turn(id) {
-
 }
