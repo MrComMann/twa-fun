b7db5958ed13fafd2593a1b60eaa638256eade73
MrComMann <ondra.plicka@seznam.cz>
2023-10-12 14:51:19 +0200

    g

--- 5/proc.js
+++ 5/proc.js
@@ -8,11 +8,13 @@ document.addEventListener('DOMContentLoaded', async ()=> {
     const data = await sendRequest('http://kuroedov.lab.uzlabina.cz/twa/api/02_slow.php');
     const parsed = JSON.parse(data)
     let a = document.getElementById('loader')
+    const ul = document.createElement("ul")
     parsed.forEach((e)=> {
         const li = document.createElement("li")
         li.innerHTML = e
-        a.append(li)
+        ul.append(li)
     })
+    a.append(ul)
     a.classList.remove("loader")
     console.log(parsed)
 })
