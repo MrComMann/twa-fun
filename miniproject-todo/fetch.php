a8d52095dbe679dcbb542c6d01df85a93aa1dd04
Ondra Plicka <ondra.plicka@seznam.cz>
2023-10-15 20:21:33 +0200

    todoo

--- miniproject-todo/fetch.php
+++ miniproject-todo/fetch.php
@@ -1,41 +1,59 @@
 <?php
-require("./db.php");
+require_once("./db.php");
 $method = $_SERVER['REQUEST_METHOD'];
 header("Content-Type: application/json");
 
-$dsn = "mysql:host=" . $host . ";dbname=" . $dbmane . ";port=3336";
+$dsn = "mysql:host=" . $host . ";dbname=" . $dbname . ";port=3306";
 $options = array(
     PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8",
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 );
 try {
     $conn = new PDO($dsn, $username, $password, $options);
+
+    if ($method === "POST") {
+        $data;
+
+        if (isset($_POST['task']) && isset($_POST['date'])) {
+            $text = $_POST['task'];
+            $date = $_POST['date'];
+            try {
+                $stmt = $conn->prepare("INSERT INTO `d323285_twa`.`TD_tasks` (`ID`, `Task`, `Time`, `Status`) VALUES (NULL, :task, :datet, '1');");
+                $stmt->bindParam(':task', $text);
+                $stmt->bindParam(':datet', $date);
+                $stmt->execute();
+                $data = $stmt->fetchAll(PDO::FETCH_OBJ);
+                $stmt = null;
+                $conn = null;
+                echo json_encode(array("status" => "success"));
+            } catch (PDOException $e) {
+                echo json_encode(array("status" => "error"));
+            }
+        } else {
+            $data = array("method" => "POST", "status" => "error");
+        }
+
+        echo json_encode($data);
+    } else if ($method === "PUT") {
+        echo json_encode(array("method" => "PUT"));
+    } else if ($method === "DELETE") {
+        echo json_encode(array("method" => "DELETE"));
+    } else if ($method === "GET") {
+        try {
+            $stmt = $conn->prepare("SELECT * FROM `TD_tasks`");
+            $stmt->execute();
+            $data = $stmt->fetchAll(PDO::FETCH_OBJ);
+            $stmt = null;
+            $conn = null;
+            echo json_encode($data);
+        } catch (PDOException $e) {
+            echo json_encode(array("status" => "error"));
+        }
+    }
+
 } catch (PDOException $e) {
     echo "Nelze se připojit k MySQL: ";
     echo $e->getMessage(); //smazat
 }
-
-//else if použito místo switche, protože je to rychlejší (údajně)
-if ($method === "POST") {
-    $data;
-    if (isset($_POST['task']) && isset($_POST['date'])) {
-        $text = $_POST['task'];
-        $date = $_POST['date'];
-        $data = array("method" => "POST", "text" => $text, "date" => $date);
-    } else {
-        $data = array("method" => "POST", "status" => "error");
-    }
-
-    echo json_encode($data);
-} else if ($method === "PUT") {
-    echo json_encode(array("method" => "PUT"));
-} else if ($method === "DELETE") {
-    echo json_encode(array("method" => "DELETE"));
-} else if ($method === "GET") {
-    echo json_encode(array("method" => "GET"));
-}
-
-
-
 exit();
 ?>
--- miniproject-todo/index.js
+++ miniproject-todo/index.js
@@ -9,24 +9,56 @@ async function sendRequest(url, data, method) {
     return text;
 }
 
+function subInit($id, $count, $btn) {
+    const btn = document.getElementById($btn)
+    document.getElementById($id).innerHTML = $count
+    if ($count > 0) {
+        btn.classList.add('active')
+    } else {
+        btn.classList.remove('active')
+    }
+}
+
 document.addEventListener('DOMContentLoaded', async () => {
+    const active = document.getElementById("activeTable")
+    const todo = document.getElementById("todoTable")
+    const done = document.getElementById("doneTable")
+
     const data = await sendRequest(URL, null, "GET");
     const parsed = JSON.parse(data)
-    console.log(parsed)
+    let activeC = 0;
+    let todoC = 0;
+    let finishedC = 0;
+    for (let task of parsed) {
+        const tr = document.createElement('tr');
+        tr.innerHTML = "<td>" + task.Task + "</td>"
+        if (task.Status == 1) {
+            todo.append(tr)
+            todoC++
+        } else if (task.Status == 2) {
+            active.append(tr)
+            activeC++
+        } else {
+            done.append(tr)
+            finishedC++
+        }
+    }
+    subInit("activeCount", activeC, "activeBtn");
+    subInit("todoCount", todoC, "todoBtn");
+    subInit("finishedCount", finishedC, "finishedBtn");
 
     document.getElementById('newTodo').addEventListener('submit', async (e) => {
         e.preventDefault();
 
         const text = document.getElementById('text').value
-        const date = document.getElementById('date').value //rrrr-mm-ddThh:mm
+        let date = document.getElementById('date').value //rrrr-mm-ddThh:mm
+        date = date.split("T")
+        date = date[0] + " " + date[1] + ":00"
 
         const formData = new FormData()
         formData.append("task", text)
         formData.append("date", date)
-
-        const req = await sendRequest(URL, formData, "POST");
-        const data = JSON.parse(req)
-        console.log(data)
+        await sendRequest(URL, formData, "POST");
 
         window.location.reload()
     })
